fd98465b0d4aeb8c2242d1d364e2eb2b8211e61f
sjoerdoudman <sjoerdoudman@users.noreply.github.com>
2026-07-01 06:42:44 +0200

    vertical space fix

--- frontend/app/components/blocks/TextImage.vue
+++ frontend/app/components/blocks/TextImage.vue
@@ -2,13 +2,13 @@
     <section class="flex flex-col gap-quadruple-space last:mb-sextuple-space">
         <div class="container--lg flex flex-col gap-triple-space">
             <div class="grid md:grid-cols-2 gap-double-space">
-                <figure class="flex flex-col gap-single-space">
+                <figure class="flex-col gap-single-space" :class="[ !data.image ? 'hidden md:flex' : 'flex' ]">
                     <ElementsImage v-if="data.image" :width="'600'" :height="'600'" :url="data.image.url" :alt="data.image.alt"></ElementsImage>
                 </figure>
                 <div class="col-span-1 flex flex-col gap-double-space">
                     <div class="flex flex-col gap-4">
-                        <h3 class="text-highlight text-[2.25rem]" v-html="data.title"></h3>
-                        <div class="editor" v-html="data.text"></div>
+                        <h3 v-if="data.title" class="text-highlight text-[2.25rem]" v-html="data.title"></h3>
+                        <div v-if="data.text" class="editor" v-html="data.text"></div>
                     </div>
                     <div v-if="data.buttons?.length" class="flex gap-6 mt-single-space">
                         <ElementsButton v-for="button in data.buttons" :data="button"></ElementsButton>
--- frontend/app/components/blocks/Title.vue
+++ frontend/app/components/blocks/Title.vue
@@ -3,8 +3,8 @@
         <div class="container--lg w-full">
             <div class="content-column">
                 <div class="col-span-2 title-block flex flex-col gap-4">
-                    <h2 class="text-highlight uppercase font-bold" v-html="$softHyphen(data.title)"></h2>
-                    <div class="intro" v-html="data.description"></div>
+                    <h2 v-if="data.title" class="text-highlight uppercase font-bold" v-html="$softHyphen(data.title)"></h2>
+                    <div v-if="data.description" class="intro" v-html="data.description"></div>
                     <div v-if="data.buttons && data.buttons.length" class="flex flex-col md:flex-row gap-6 mt-single-space">
                         <ElementsButton v-for="button in data.buttons" :data="button"></ElementsButton>
                     </div>
